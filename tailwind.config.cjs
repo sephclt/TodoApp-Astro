1285f13612af727a8b7c1972752891aca9eb28e2
sephclt <josephajero22@gmail.com>
2023-02-17 15:35:52 +0800

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  daisyui: {
+    theme: ['night'],
+  },
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+};
